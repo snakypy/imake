ffd38e186d034aa4d0d359f560d9f2fab9a60e3c
William C. Canin <william.costa.canin@gmail.com>
2021-05-25 04:04:13 -0300

    :hammer: :bug: Correcting error of object call in tests

--- tests/test_imake.py
+++ tests/test_imake.py
@@ -13,4 +13,4 @@ def test_version():
     pyproject = join(getcwd(), "pyproject.toml")
     if exists(pyproject):
         version_toml = toml_parsed(read_file(pyproject))["tool"]["poetry"]["version"]
-        assert version_toml == imake.Imake.INFO["version"]
+        assert version_toml == imake.Main.INFO["version"]
